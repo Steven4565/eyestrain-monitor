478c0595b0e9ec0e4bc2ed3ed86505237594ad11
NathanAdhitya <me@natha.my.id>
2022-07-30 18:54:33 +0700

    move load config after tkinter init

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ from source.utils.Config import *
 # Tkinter imports
 from source.customWidgets import *
 
-AppConfig.load_config()
-
-
 def main():
 
     # =========== Main Window ===========
     AppGuiInstance = AppGui()
+    AppConfig.load_config()
+    
     AppGuiInstance.init_window('EyeStrain Monitor')
     AppGuiInstance.init_menu()
     AppGuiInstance.init_pages()
+    
 
     AppGuiInstance.init_videostream(AppConfig.cfg["video"]["camera_index"])
     AppGuiInstance.update_canvas()
